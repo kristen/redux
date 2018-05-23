4919a32eb3edda4938a81a5f7788548e131b49d7
Kristen Sundquist <kristen@sigfig.com>
2018-05-23 14:31:41 -0700

    Redux: Reducer Composiition with Objects

--- todo-list/main.js
+++ todo-list/main.js
@@ -32,8 +32,33 @@ const todos = (state = [], action) => {
     }
 };
 
+const visibilityFilter = (
+    state = 'SHOW_ALL',
+    action
+) => {
+    switch (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+}
+
+const todoApp = (state = {}, action) => {
+    return {
+        todos: todos(
+            state.todos,
+            action
+        ),
+        visibilityFilter: visibilityFilter(
+            state.visibilityFilter,
+            action
+        )
+    };
+};
+
 const { createStore } = Redux;
-const store = createStore(todos);
+const store = createStore(todoApp);
 
 console.log('Initial state:');
 console.log(store.getState());
@@ -63,6 +88,15 @@ console.log('Dispatching TOGGLE_TODO.');
 store.dispatch({
     type: 'TOGGLE_TODO',
     id: 0
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('-----------------');
+
+console.log('Dispatching SET_VISIBILITY_FILTER.');
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_COMPLETED'
 });
 console.log('Current state:');
 console.log(store.getState());
