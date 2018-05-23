e37d0285e1cc68c520f9035fbbfaf65b16f3fb69
Kristen Sundquist <kristen@sigfig.com>
2018-05-23 14:26:57 -0700

    Add Redux store and examples to todo list

--- todo-list/main.js
+++ todo-list/main.js
@@ -32,6 +32,43 @@ const todos = (state = [], action) => {
     }
 };
 
+const { createStore } = Redux;
+const store = createStore(todos);
+
+console.log('Initial state:');
+console.log(store.getState());
+console.log('-----------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('-----------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 1,
+    text: 'Go Shopping'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('-----------------');
+
+console.log('Dispatching TOGGLE_TODO.');
+store.dispatch({
+    type: 'TOGGLE_TODO',
+    id: 0
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('-----------------');
+
+
 const testAddTodo = () => {
     const stateBefore = [];
     const action = {
