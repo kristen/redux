46b6243e2661f756b92cc048db9ffa3be4385c07
Kristen Sundquist <kristen@sigfig.com>
2018-05-24 13:36:52 -0700

    Move tests to separate file

---
+++ todo-list/test.js
@@ -0,0 +1,109 @@
+console.log('Initial state:');
+console.log(store.getState());
+console.log('-----------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('-----------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 1,
+    text: 'Go Shopping'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('-----------------');
+
+console.log('Dispatching TOGGLE_TODO.');
+store.dispatch({
+    type: 'TOGGLE_TODO',
+    id: 0
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('-----------------');
+
+console.log('Dispatching SET_VISIBILITY_FILTER.');
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_COMPLETED'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('-----------------');
+
+
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Learn Redux'
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
+const testToggleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go shopping',
+            completed: false
+        }
+    ];
+
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    };
+
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go shopping',
+            completed: true
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+}
+
+testAddTodo();
+testToggleTodo();
+console.log('All tests passed.');
