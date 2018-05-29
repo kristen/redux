b25f8f91b6af59f9c6931c799a198b09c6cc3cab
Kristen Sundquist <kristen@sigfig.com>
2018-05-29 15:14:34 -0700

    Use connect from ReactRedux to generate containers (FilterLink)

--- todo-list/main.js
+++ todo-list/main.js
@@ -157,44 +157,32 @@ const Link = ({
     );
 };
 
-class FilterLink extends Component {
-    componentDidMount() {
-        const { store } = this.context;
-        // subscribe to store so component gets rerendered
-        this.unsubscribe = store.subscribe(() =>
-            this.forceUpdate()
-        );
-    }
-
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-
-    render () {
-        const props = this.props;
-        const { store } = this.context;
-        const state = store.getState();
-
-        return (
-            <Link
-                active={
-                    props.filter === state.visibilityFilter
-                }
-                onClick={() => 
-                    store.dispatch({
-                        type: 'SET_VISIBILITY_FILTER',
-                        filter: props.filter
-                    })
-                }
-            >
-                {props.children}
-            </Link>
-        );
-    }
-}
-FilterLink.contextTypes = {
-    store: React.PropTypes.object
+const mapStateToFilterLinkProps = (
+    state,
+    ownProps
+) => {
+    return {
+        active: ownProps.filter === state.visibilityFilter
+    };
 };
+
+const mapDispatchToFilterLinkProps = (
+    dispatch,
+    ownProps
+) => {
+    return {
+        onClick: () => {
+            dispatch({
+                type: 'SET_VISIBILITY_FILTER',
+                filter: ownProps.filter
+            });
+        }
+    };
+};
+const FilterLink = connect(
+    mapStateToFilterLinkProps,
+    mapDispatchToFilterLinkProps
+)(Link);
 
 const Footer = () => {
     return (
