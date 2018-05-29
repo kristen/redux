20cb035893dc2b871b76b6070cb0bad2665d2b51
Kristen Sundquist <kristen@sigfig.com>
2018-05-29 11:57:26 -0700

    Refactor: Extract Container Components (FilterLink)
    Avoid the boilerplate of passing the props down the intermediate components by introducing more container components

--- todo-list/main.js
+++ todo-list/main.js
@@ -69,27 +69,6 @@ const store = createStore(todoApp);
 
 const { Component } = React;
 
-const FilterLink = ({
-    filter,
-    currentFilter,
-    children,
-    onClick
-}) => {
-    if (filter === currentFilter) {
-        return <span>{children}</span>;
-    }
-    return (
-        <a href='#'
-           onClick={e => {
-               e.preventDefault();
-               onClick(filter);
-           }}
-        >
-            {children}
-        </a>
-    );
-};
-
 const getVisibileTodos = (
     todos,
     filter
@@ -156,32 +135,77 @@ const AddTodo = ({
     );
 }
 
-const Footer = ({
-    visibilityFilter,
-    onFilterClick
+const Link = ({
+    active,
+    children,
+    onClick
 }) => {
+    if (active) {
+        return <span>{children}</span>;
+    }
+    return (
+        <a href='#'
+           onClick={e => {
+               e.preventDefault();
+               onClick();
+           }}
+        >
+            {children}
+        </a>
+    );
+};
+
+class FilterLink extends Component {
+    componentDidMount() {
+        // subscribe to store so component gets rerendered
+        this.unsubscribe = store.subscribe(() =>
+            this.forceUpdate()
+        );
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render () {
+        const props = this.props;
+        const state = store.getState();
+
+        return (
+            <Link
+                active={
+                    props.filter === state.visibilityFilter
+                }
+                onClick={() => 
+                    store.dispatch({
+                        type: 'SET_VISIBILITY_FILTER',
+                        filter: props.filter
+                    })
+                }
+            >
+                {props.children}
+            </Link>
+        );
+    }
+}
+
+const Footer = () => {
     return (
         <p>
             Show:
             {' '}
             <FilterLink
-                filter='SHOW_ALL'
-                currentFilter={visibilityFilter}
-                onClick={onFilterClick}>
+                filter='SHOW_ALL'>
                 All
             </FilterLink>
-            {' '}
+            {', '}
             <FilterLink
-                filter='SHOW_ACTIVE'
-                currentFilter={visibilityFilter}
-                onClick={onFilterClick}>
+                filter='SHOW_ACTIVE'>
                 Active
             </FilterLink>
-            {' '}
+            {', '}
             <FilterLink
-                filter='SHOW_COMPLETED'
-                currentFilter={visibilityFilter}
-                onClick={onFilterClick}>
+                filter='SHOW_COMPLETED'>
                 Completed
             </FilterLink>
         </p>
@@ -216,14 +240,7 @@ const TodoApp = ({
                     id
                 })
             } />
-        <Footer
-            visibilityFilter={visibilityFilter}
-            onFilterClick={filter =>
-                store.dispatch({
-                    type: 'SET_VISIBILITY_FILTER',
-                    filter
-                })
-            } />
+        <Footer />
     </div>
 );
 
