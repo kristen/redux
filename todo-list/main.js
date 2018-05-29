72157e1e815886362309c7a199c3165fc1819f59
Kristen Sundquist <kristen@sigfig.com>
2018-05-29 11:18:38 -0700

    Refactor: Make Todo and TodoList components

--- todo-list/main.js
+++ todo-list/main.js
@@ -108,6 +108,37 @@ const getVisibileTodos = (
     }
 }
 
+const Todo = ({
+    onClick,
+    completed,
+    text
+}) => (
+    <li onClick={onClick}
+        style={{
+            textDecoration:
+            completed ?
+                'line-through' :
+                'none'
+        }}>
+        {text}
+    </li>
+);
+
+const TodoList = ({
+    todos,
+    onTodoClick
+}) => (
+    <ul>
+        {todos.map(todo => 
+            <Todo
+                key={todo.id}
+                {...todo}
+                onClick={() => onTodoClick(todo.id)}
+            />
+        )}
+    </ul>
+);
+
 let nextTodoId = 0;
 class TodoApp extends Component {
     render() {
@@ -134,25 +165,14 @@ class TodoApp extends Component {
                 }}>
                     Add Todo
                 </button>
-                <ul>
-                    {visibleTodos.map(todo =>
-                        <li key={todo.id}
-                            onClick={() => {
-                                store.dispatch({
-                                    type: 'TOGGLE_TODO',
-                                    id: todo.id
-                                });
-                            }}
-                            style={{
-                                textDecoration:
-                                todo.completed ?
-                                    'line-through' :
-                                    'none'
-                            }}>
-                            {todo.text}
-                        </li>
-                    )}
-                </ul>
+                <TodoList
+                    todos={visibleTodos}
+                    onTodoClick={id =>
+                        store.dispatch({
+                            type: 'TOGGLE_TODO',
+                            id
+                        })
+                    } />
                 <p>
                     Show:
                     {' '}
