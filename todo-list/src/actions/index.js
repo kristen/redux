d6bda8fac52e7d79239543fec6b471bc8c9d5921
Kristen Sundquist <kristen@sigfig.com>
2018-05-30 11:24:56 -0700

    Persist state in Local Storage

--- todo-list/src/actions/index.js
+++ todo-list/src/actions/index.js
@@ -1,9 +1,9 @@
-// action creators
+import { v4 } from 'node-uuid';
 
-let nextTodoId = 0;
+// action creators
 export const addTodo = (text) => ({
     type: 'ADD_TODO',
-    id: (nextTodoId++).toString(),
+    id: v4(),
     text,
 });
 
--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -3,25 +3,23 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
+// only from a file called throttle because we don't want all of lodash
+import throttle from 'lodash/throttle';
 import todoApp from './reducers';
 import TodoApp from './components/TodoApp';
+import { loadState, saveState } from './localStorage';
 
-const persistedState = {
-    todos: [{
-        id: '0',
-        text: 'Learn React',
-        completed: false,
-    }, {
-        id: '1',
-        text: 'Learn Redux',
-        completed: false,
-    }],
-};
+const persistedState = loadState();
 const store = createStore(
     todoApp,
     persistedState
 );
-console.log(store.getState());
+
+store.subscribe(throttle(() => {
+    saveState({
+        todos: store.getState().todos
+    });
+}), 1000);
 
 render(
     <Provider store={store}>
